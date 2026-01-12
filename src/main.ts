bd5c17692c8274ff744a715d59bbe991bdb7262c
Ken <ken@ausmed.com.au>
2026-01-12 16:19:43 +1100

    remove cors for development

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,9 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // Allow local dev frontend (ng serve) to call the API.
-  // Tighten this for production as needed.
-  app.enableCors();
+  // // Allow local dev frontend (ng serve) to call the API.
+  // // Tighten this for production as needed.
+  // app.enableCors();
 
   app.useGlobalPipes(
     new ValidationPipe({
