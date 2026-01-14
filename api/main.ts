026d9613ea19b4eb78b2946e88f897154ed61340
Ken <ken@ausmed.com.au>
2026-01-14 18:09:18 +1100

    fix: deployment

--- api/main.ts
+++ api/main.ts
@@ -1,14 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { ExpressAdapter } from '@nestjs/platform-express';
 import express from 'express';
-import { AppModule } from 'src/app.module';
+
+// ⚠️ 注意：这里一定是 dist
+import { AppModule } from '../dist/app.module';
 
 const server = express();
-
-let isBootstrapped = false;
+let isInitialized = false;
 
 async function bootstrap() {
-  if (isBootstrapped) return;
+  if (isInitialized) return;
 
   const app = await NestFactory.create(
     AppModule,
@@ -17,7 +18,7 @@ async function bootstrap() {
 
   await app.init();
 
-  isBootstrapped = true;
+  isInitialized = true;
 }
 
 bootstrap();
